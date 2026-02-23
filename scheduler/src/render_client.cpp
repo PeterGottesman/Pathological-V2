cb0ea6ecdeadfc8fac4836821c577490208f90b3
Hunter Ellenberger <helly10@outlook.com>
2026-02-23 17:24:55 -0500

    added render_client and render_server files to handle their grpc request

---
+++ scheduler/src/render_client.cpp
@@ -0,0 +1,80 @@
+#include <cstdint>
+#include <grpcpp/grpcpp.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "../build/protos/render_server.grpc.pb.h"
+#include "protos/render_server.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using render_server::RenderWorker;
+using render_server::RenderJobRequest;
+using render_server::RenderJobResponse;
+using render_server::RenderStatusRequest;
+using render_server::RenderStatusResponse;
+
+// foobar struct for sending data
+struct myData {
+    std::string scene_location;
+    uint32_t seconds;
+    uint32_t frames;
+};
+
+class RenderWorkerClient {
+ public:
+  RenderWorkerClient(std::shared_ptr<Channel> channel)
+      : stub_(RenderWorker::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  int RenderJob(myData testStructure) {
+    // Data we are sending and getting back
+    RenderJobRequest request;
+    RenderJobResponse response;
+    ClientContext context;
+
+    // Adds data to request
+    request.set_scene_location(testStructure.scene_location);
+    request.set_seconds(testStructure.seconds);
+    request.set_frames(testStructure.frames);
+
+    // The actual RPC.
+    Status status = stub_->RenderJob(&context, request, &response);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return response.job_identifier();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return -1;
+    }
+  }
+
+ private:
+  std::unique_ptr<RenderWorker::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Sample data
+  struct myData testStruct;
+  testStruct.scene_location = "testing";
+  testStruct.frames = 60;
+  testStruct.seconds = 2;
+
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created.
+  std::string target_str = "localhost:50051";
+
+  // We indicate that the channel isn't authenticated (use of
+  // InsecureChannelCredentials()).
+  RenderWorkerClient client(
+      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+  int response = client.RenderJob(testStruct);
+  std::cout << "Greeter received: " << response << std::endl;
+  return 0;
+}
---
+++ scheduler/src/render_server.cpp
@@ -0,0 +1,58 @@
+#include <grpcpp/grpcpp.h>
+#include "../build/protos/render_server.grpc.pb.h"
+#include "protos/render_server.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using render_server::RenderWorker;
+using render_server::RenderJobRequest;
+using render_server::RenderJobResponse;
+using render_server::RenderStatusRequest;
+using render_server::RenderStatusResponse;
+
+#include <iostream>
+#include <string>
+
+class SchedulerServer final : public RenderWorker::Service {
+    Status RenderJob(ServerContext *context, const RenderJobRequest *request, RenderJobResponse *response) {
+        std::cout << "Scene location: " << request->scene_location() << std::endl;
+        std::cout << "Frames: " << request->frames() << std::endl;
+        std::cout << "Seconds: " << request->seconds() << std::endl;
+        response->set_job_identifier(rand() % 10000);
+        return Status::OK;
+    }
+
+    Status RenderStatus(ServerContext *context, const RenderStatusRequest *request, RenderStatusResponse *response) {
+        // Implementation here
+        return Status::OK;
+    }
+};
+
+void RunServer(uint16_t port) {
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  SchedulerServer service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  ServerBuilder builder;
+
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+  // Register "service" as the instance through which we'll communicate with
+  // clients.
+  builder.RegisterService(&service);
+
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown
+  server->Wait();
+}
+
+int main(int argc, char *argv[]) {
+    RunServer(50051);
+    return 0;
+}
