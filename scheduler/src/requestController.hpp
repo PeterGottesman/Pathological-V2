4140ba2babcf108dbe626f57fcca4db953d62687
austinjohnson03 <amj102998@gmail.com>
2026-03-01 11:44:09 -0500

    finished controller HTTP methods. ready for tests.
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- scheduler/src/requestController.hpp
+++ scheduler/src/requestController.hpp
@@ -9,21 +9,22 @@ using namespace drogon;
 
 // Maybe frame render and animation render?
 
-class RequestController : public drogon::HttpController<RequestController>
-{
+class RequestController : public drogon::HttpController<RequestController> {
 public:
-    METHOD_LIST_BEGIN
-    // Method sends status of a render back to the client
-    ADD_METHOD_TO(RequestController::getStatus, "/renders/{1}/status", Get); // Path is /renders/{id}/status
-    // Method receives render from the client and creates a render request.
-    ADD_METHOD_TO(RequestController::createRenderRequest, "/renders", Post);
-    METHOD_LIST_END
+  METHOD_LIST_BEGIN
+  // Method sends status of a render back to the client
+  ADD_METHOD_TO(RequestController::getStatus, "/renders/{1}/status",
+                Get); // Path is /renders/{id}/status
+  // Method receives render from the client and creates a render request.
+  ADD_METHOD_TO(RequestController::createRenderRequest, "/renders", Post);
+  METHOD_LIST_END
 
-    void getStatus(const HttpRequestPtr &req,
-                   std::function<void(const HttpResponsePtr &)> &&callback,
-                   int id) const;
+  void getStatus(const HttpRequestPtr &req,
+                 std::function<void(const HttpResponsePtr &)> &&callback,
+                 int id) const;
 
-    void createRenderRequest(const HttpRequestPtr &req,
-                             std::function<void(const HttpResponsePtr &)> &&callback,
-                             RenderRequest::RenderRequest &&pRequest);
+  void
+  createRenderRequest(const HttpRequestPtr &req,
+                      std::function<void(const HttpResponsePtr &)> &&callback,
+                      Json::Value &&payload);
 };
