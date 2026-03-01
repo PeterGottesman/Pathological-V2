b07ec629d4011818a2277cde28eb822db59b5cb8
austinjohnson03 <amj102998@gmail.com>
2026-03-01 11:45:16 -0500

    started basic drogon application
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- scheduler/src/main.cpp
+++ scheduler/src/main.cpp
@@ -7,17 +7,19 @@ int main(int argc, char **argv) {
   CLI::App app{"Pathological Scheduler"};
 
   bool some_option;
-  drogon::app().addListener("0.0.0.0", 80);
+  drogon::app().addListener("0.0.0.0", 8080);
 
   // Likely options:
   // - ips/ports to listen on
   // - database connections
   // - any config files
   // - ...
-  app.add_option("someoption", some_option, "Some fake option")->required();
+  // app.add_option("someoption", some_option, "Some fake option")->required();
   CLI11_PARSE(app, argc, argv);
 
   Scheduler sched{some_option};
+
+  // Blocking call. Everything must go above this.
   drogon::app().run();
   return 0;
 }
