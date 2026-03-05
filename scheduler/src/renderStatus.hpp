bb4e8aaa2b1bc02de23d1a6ed32caccc657fc861
austinjohnson03 <amj102998@gmail.com>
2026-03-05 13:09:13 -0500

    fix: changed default from 'unknown' to 'error'
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- scheduler/src/renderStatus.hpp
+++ scheduler/src/renderStatus.hpp
@@ -1,27 +1,21 @@
 #pragma once
 
-// Could maybe add 'created' state upon creation of the request
-enum class RenderStatus
-{
-    COMPLETED,
-    IN_PROGRESS,
-    IN_QUEUE,
-    ERROR,
-};
+#include <string>
 
-inline std::string renderStatusToString(RenderStatus status)
-{
-    switch (status)
-    {
-    case RenderStatus::COMPLETED:
-        return "Completed";
-    case RenderStatus::IN_PROGRESS:
-        return "In Progress";
-    case RenderStatus::IN_QUEUE:
-        return "In Queue";
-    case RenderStatus::ERROR:
-        return "Error";
-    default:
-        return "Unknown";
-    }
+// Could maybe add 'created' state upon creation of the request
+enum class RenderStatus { COMPLETED, IN_PROGRESS, IN_QUEUE, ERROR };
+
+inline std::string renderStatusToString(RenderStatus status) {
+  switch (status) {
+  case RenderStatus::COMPLETED:
+    return "Completed";
+  case RenderStatus::IN_PROGRESS:
+    return "In Progress";
+  case RenderStatus::IN_QUEUE:
+    return "In Queue";
+  case RenderStatus::ERROR:
+    return "Error";
+  default:
+    return "Error";
+  }
 }
