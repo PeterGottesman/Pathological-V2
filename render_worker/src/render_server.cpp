94dd98a37c54d5d0f89fbbd8544c4101072a2875
Hunter Ellenberger <helly10@outlook.com>
2026-03-06 01:49:22 -0500

    changed test response to COMPLETED instead of done to reflect changes in rest C++

--- render_worker/src/render_server.cpp
+++ render_worker/src/render_server.cpp
@@ -2,28 +2,24 @@
 #include <iostream>
 #include <string>
 
-Status SchedulerServer::RenderJob(ServerContext *context,
-                                  const RenderJobRequest *request,
-                                  RenderJobResponse *response) {
-  // Sample implementation
-  std::cout << "Scene location: " << request->scene_location() << std::endl;
-  std::cout << "Frames: " << request->frames() << std::endl;
-  std::cout << "Seconds: " << request->seconds() << std::endl;
-  response->set_job_identifier(rand() % 10000);
-  return Status::OK;
+Status SchedulerServer::RenderJob(ServerContext *context, const RenderJobRequest *request, RenderJobResponse *response) {
+    // Sample implementation
+    std::cout << "Scene location: " << request->scene_location() << std::endl;
+    std::cout << "Frames: " << request->frames() << std::endl;
+    std::cout << "Seconds: " << request->seconds() << std::endl;
+    response->set_job_identifier(rand() % 10000);
+    return Status::OK;
 }
 
-Status SchedulerServer::RenderStatus(ServerContext *context,
-                                     const RenderStatusRequest *request,
-                                     RenderStatusResponse *response) {
-  // Sample implementation
-  response->set_status(render_server::DONE);
-  return Status::OK;
+Status SchedulerServer::RenderStatus(ServerContext *context, const RenderStatusRequest *request, RenderStatusResponse *response) {
+    // Sample implementation
+    response->set_status(render_server::COMPLETED);
+    return Status::OK;
 }
 
-void RunServer(uint16_t port, SchedulerClient &client, std::string worker_id) {
+void RunServer(uint16_t port) {
   std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-  RenderServer service(client, worker_id);
+  SchedulerServer service;
 
   grpc::EnableDefaultHealthCheckService(true);
   ServerBuilder builder;
