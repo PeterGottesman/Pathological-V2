fed03e6e8ff0a947b9dcc4926d92a5cf024a6de4
austinjohnson03 <amj102998@gmail.com>
2026-03-08 16:07:50 -0400

    finished scheduler s3 manager class.
    - removed ability to hardcode credentials.
      - credentials must be loaded from ~/.aws/credentials/ directory.
    
    - removed GET and PUT object functions since scheduler only validates
      file exists in Scheduler before sending to Render Worker.
    
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- scheduler/src/s3Manager.cpp
+++ scheduler/src/s3Manager.cpp
@@ -1,29 +1,13 @@
-#include <aws/core/auth/AWSCredentialsProvider.h>
-#include <aws/s3/model/GetObjectRequest.h>
 #include <aws/s3/model/HeadObjectRequest.h>
-#include <aws/s3/model/ListObjectsV2Request.h>
-#include <aws/s3/model/PutObjectRequest.h>
-#include <filesystem>
-#include <fstream>
 #include <iostream>
 
 #include "s3Manager.hpp"
 
 S3Manager::S3Manager(const S3Config &config) : config(config) {
   Aws::InitAPI(options);
-
   Aws::Client::ClientConfiguration clientConfig;
-
-  clientConfig.region = config.reigon;
-
-  if (!config.acceessKeyId.empty() && !config.secretKey.empty()) {
-    Aws::Auth::AWSCredentials credentials(config.acceessKeyId,
-                                          config.secretKey);
-
-    client = std::make_unique<Aws::S3::S3Client>(credentials, clientConfig);
-  } else {
-    client = std::make_unique<Aws::S3::S3Client>(clientConfig);
-  }
+  clientConfig.region = config.region;
+  client = std::make_unique<Aws::S3::S3Client>(clientConfig);
 }
 
 S3Manager::~S3Manager() {
@@ -31,28 +15,25 @@ S3Manager::~S3Manager() {
   Aws::ShutdownAPI(options);
 }
 
-bool S3Manager::validateFile(const std::string &s3Path) {
+bool S3Manager::keyExists(const std::string &s3Key) {
   Aws::S3::Model::HeadObjectRequest req;
   req.SetBucket(config.bucketName);
-  req.SetKey(s3Path);
+  req.SetKey(s3Key);
 
   auto resp = client->HeadObject(req);
   if (resp.IsSuccess()) {
     return true;
   }
 
-  std::cerr << "Validation request failed for '" << s3Path
+  std::cerr << "Validation request failed for '" << s3Key
             << "': " << resp.GetError().GetMessage() << std::endl;
 
   return false;
 }
 
-std::optional<std::string>
-S3Manager::requestSceneFile(const std::string &s3Path) {}
-
-bool S3Manager::searchFiles(const std::string &s3Path) {}
-
-std::optional<std::string>
-S3Manager::requestOutputFile(const std::string &s3Path) {}
-
-std::string S3Manager::extractFilename(const std::string &path) {}
+std::optional<std::string> S3Manager::requestFileUrl(const std::string &s3Key) {
+  auto url = client->GeneratePresignedUrl(config.bucketName, s3Key,
+                                          Aws::Http::HttpMethod::HTTP_GET,
+                                          kPresignedUrlTimeout);
+  return url.empty() ? std::nullopt : std::optional<std::string>(url);
+}
