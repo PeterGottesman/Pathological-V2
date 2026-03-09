2bb405d460e251d9be548444030efd6eb8e0dba2
Hunter Ellenberger <helly10@outlook.com>
2026-03-09 03:11:08 -0400

    added cli options; made sure address and port is sent to scheduler

--- render_worker/src/main.cpp
+++ render_worker/src/main.cpp
@@ -3,6 +3,7 @@
 #include "scheduler_client.hpp"
 #include <CLI/CLI.hpp>
 
+#include <cstdint>
 #include <iostream>
 #include <string>
 
@@ -17,20 +18,24 @@ Status SchedulerServer::RenderJob(ServerContext *context, const RenderJobRequest
 int main(int argc, char** argv) {
     CLI::App app{"Pathological - Vulkan Path Tracer"};
 
-    std::string schedulerAddress = "localhost:50051";
-    app.add_option("address", schedulerAddress, "Address and port to find scheduler at")->required();
+    std::string schedulerAddress;
+    std::string renderServerAddress = "127.0.0.1";
+    uint32_t renderServerPort = 50051;
 
-    RunServer(50051);
+    app.add_option("schedulerAddress", schedulerAddress, "Address and port to find scheduler at")->required();
+    app.add_option("-a,--render-address", renderServerAddress, "Address of current machine running program")->default_val("127.0.0.1");
+    app.add_option("-p,--port", renderServerPort, "Port to launch server")->default_val(50051);
+    CLI11_PARSE(app, argc, argv);
 
-    std::string target_str = "localhost:50051";
-    // We indicate that the channel isn't authenticated (use of
-    // InsecureChannelCredentials()).
-    SchedulerClient client(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+    SchedulerClient client(grpc::CreateChannel(schedulerAddress, grpc::InsecureChannelCredentials()));
 
-    // Giving sample connection address
-    //std::string connection_address = "127.0.0.1:50051";
-    //int response = client.EstablishConnection(connection_address);
-    //std::cout << "Greeter received: " << response << std::endl;
+    // Sends scheduler IP and port
+    int response = client.EstablishConnection(
+        renderServerAddress + ":" + std::to_string(renderServerPort)
+    );
+    std::cout << "Greeter received: " << response << std::endl;
+
+    RunServer(renderServerPort);
 
     return 0;
 }
