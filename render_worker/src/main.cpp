698f52c4b3ef0f49ba529aa572524387bfee6f03
Hunter Ellenberger <helly10@outlook.com>
2026-03-22 20:49:28 -0400

    added more verbose error message

--- render_worker/src/main.cpp
+++ render_worker/src/main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char** argv) {
     );
 
     if(client.EstablishConnection() == 1){
+        std::cerr << "Could not establish connection with scheduler. Now exiting." << std::endl;
         return 1;
     }
 
