eb13f1b03bbfc7cc1d3479d28c877ed200085721
Hunter Ellenberger <helly10@outlook.com>
2026-03-22 23:50:36 -0400

    RenderJob has been moved back for readability; Class now stores worker id and reference to connected Scheduler client; RenderJob now sends JobCompleted alert after its render is done

--- render_worker/src/render_server.cpp
+++ render_worker/src/render_server.cpp
@@ -1,16 +1,28 @@
 #include "render_server.hpp"
+#include "render_worker.hpp"
+#include "scheduler_client.hpp"
 #include <iostream>
 #include <string>
 
-Status SchedulerServer::RenderStatus(ServerContext *context, const RenderStatusRequest *request, RenderStatusResponse *response) {
+Status RenderServer::RenderJob(ServerContext *context, const RenderJobRequest *request,
+    RenderJobResponse *response) {
+    std::cout << "Render Recieved" << std::endl;
+    response->set_job_identifier(rand() % 10000);
+    generateScene(request->width(), request->height(), request->samples(),
+        request->scene_location(), request->output_name(), request->time());
+    this->client.JobCompleted(response->job_identifier());
+    return Status::OK;
+}
+
+Status RenderServer::RenderStatus(ServerContext *context, const RenderStatusRequest *request, RenderStatusResponse *response) {
     // Sample implementation
     response->set_status(render_server::COMPLETED);
     return Status::OK;
 }
 
-void RunServer(uint16_t port) {
+void RunServer(uint16_t port, SchedulerClient& client, std::string worker_id) {
   std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-  SchedulerServer service;
+  RenderServer service(client, worker_id);
 
   grpc::EnableDefaultHealthCheckService(true);
   ServerBuilder builder;
