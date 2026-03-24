e7a4a5fc623ac6db9a2f26bf2699b938432f7b6b
austinjohnson03 <155539986+austinjohnson03@users.noreply.github.com>
2026-03-24 12:02:32 -0400

    Revert "added cli options; made sure address and port is sent to scheduler"
    This reverts commit 60e4357c34ad1b42e3f897a2d27891e3a367b8cd.

--- render_worker/src/main.cpp
+++ render_worker/src/main.cpp
@@ -3,7 +3,6 @@
 #include "scheduler_client.hpp"
 #include <CLI/CLI.hpp>
 
-#include <cstdint>
 #include <iostream>
 #include <string>
 
@@ -18,24 +17,20 @@ Status SchedulerServer::RenderJob(ServerContext *context, const RenderJobRequest
 int main(int argc, char** argv) {
     CLI::App app{"Pathological - Vulkan Path Tracer"};
 
-    std::string schedulerAddress;
-    std::string renderServerAddress = "127.0.0.1";
-    uint32_t renderServerPort = 50051;
+    std::string schedulerAddress = "localhost:50051";
+    app.add_option("address", schedulerAddress, "Address and port to find scheduler at")->required();
 
-    app.add_option("schedulerAddress", schedulerAddress, "Address and port to find scheduler at")->required();
-    app.add_option("-a,--render-address", renderServerAddress, "Address of current machine running program")->default_val("127.0.0.1");
-    app.add_option("-p,--port", renderServerPort, "Port to launch server")->default_val(50051);
-    CLI11_PARSE(app, argc, argv);
+    RunServer(50051);
 
-    SchedulerClient client(grpc::CreateChannel(schedulerAddress, grpc::InsecureChannelCredentials()));
+    std::string target_str = "localhost:50051";
+    // We indicate that the channel isn't authenticated (use of
+    // InsecureChannelCredentials()).
+    SchedulerClient client(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
 
-    // Sends scheduler IP and port
-    int response = client.EstablishConnection(
-        renderServerAddress + ":" + std::to_string(renderServerPort)
-    );
-    std::cout << "Greeter received: " << response << std::endl;
-
-    RunServer(renderServerPort);
+    // Giving sample connection address
+    //std::string connection_address = "127.0.0.1:50051";
+    //int response = client.EstablishConnection(connection_address);
+    //std::cout << "Greeter received: " << response << std::endl;
 
     return 0;
 }
