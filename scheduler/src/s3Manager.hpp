d0429c3a1f4c30b7d930826dea3c838ad93bf095
austinjohnson03 <155539986+austinjohnson03@users.noreply.github.com>
2026-03-24 12:02:32 -0400

    Revert "finished scheduler s3 manager class."
    This reverts commit 11007266a07524774129e70d5f7688788a9d636a.

--- scheduler/src/s3Manager.hpp
+++ scheduler/src/s3Manager.hpp
@@ -2,27 +2,30 @@
 
 #include <aws/core/Aws.h>
 #include <aws/s3/S3Client.h>
-#include <cstdint>
 #include <memory>
 #include <optional>
 #include <string>
 
 struct S3Config {
   std::string bucketName;
-  std::string region;
+  std::string reigon;
+  std::string acceessKeyId;
+  std::string secretKey;
 };
 
 class S3Manager {
 public:
   explicit S3Manager(const S3Config &config);
   ~S3Manager();
-  bool keyExists(const std::string &s3Key);
-  std::optional<std::string> requestFileUrl(const std::string &s3Key);
-
-  static constexpr uint64_t kPresignedUrlTimeout = 1200;
+  bool validateFile(const std::string &s3Path);
+  std::optional<std::string> requestSceneFile(const std::string &s3Path);
+  bool searchFiles(const std::string &s3Path);
+  std::optional<std::string> requestOutputFile(const std::string &s3Path);
 
 private:
   S3Config config;
   Aws::SDKOptions options;
   std::unique_ptr<Aws::S3::S3Client> client;
+
+  std::string extractFilename(const std::string &path);
 };
