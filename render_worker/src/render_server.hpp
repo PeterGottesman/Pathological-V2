556a5cefee03d5ef11fdda2ad667600137c83565
Hunter Ellenberger <helly10@outlook.com>
2026-03-29 21:35:37 -0400

    RenderServer now has boost random generator as private variable; RunServer method now take this random generator as a parameter to initalize RenderServer with it; RenderServer now has instance of RenderJobs as private variable; RenderServer::RenderJob now updates RenderJobs map with job and its status

--- render_worker/src/render_server.hpp
+++ render_worker/src/render_server.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
 #include <grpcpp/grpcpp.h>
 #include <memory>
+#include <boost/uuid.hpp>
 #include "../build/protos/render_server.grpc.pb.h"
 #include "../build/protos/render_server.pb.h"
 #include "scheduler_client.hpp"
+#include "jobs.hpp"
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -15,10 +19,14 @@ using render_server::RenderJobRequest;
 using render_server::RenderJobResponse;
 using render_server::RenderStatusRequest;
 using render_server::RenderStatusResponse;
+using boost::uuids::random_generator;
+using boost::uuids::uuid;
+using boost::uuids::to_string;
 
 class RenderServer final : public RenderWorker::Service {
 public:
-    RenderServer(SchedulerClient& client, std::string worker_id) : client(client), worker_id(worker_id){}
+    RenderServer(SchedulerClient& client, std::string worker_id, random_generator random_gen) :
+        client(client), worker_id(worker_id), random_gen(random_gen){}
     Status RenderJob(ServerContext *context, const RenderJobRequest *request,
         RenderJobResponse *response);
     Status RenderStatus(ServerContext *context, const RenderStatusRequest *request, RenderStatusResponse *response);
@@ -26,6 +34,8 @@ public:
 private:
     SchedulerClient& client;
     std::string worker_id;
+    random_generator random_gen;
+    RenderJobs jobs;
 };
 
-std::unique_ptr<Server> BuildServer(uint16_t port, SchedulerClient& client, std::string worker_id);
+std::unique_ptr<Server> BuildServer(uint16_t port, SchedulerClient& client, std::string worker_id, random_generator random_gen);
