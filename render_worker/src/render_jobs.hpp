e726635161ebcdcc19b889fbbd06690ad28f9d59
Hunter Ellenberger <helly10@outlook.com>
2026-03-31 15:54:47 -0400

    now takes shared pointer to job instead of just status; can now print all jobs in jobMap

--- render_worker/src/render_jobs.hpp
+++ render_worker/src/render_jobs.hpp
@@ -1,12 +1,15 @@
 #pragma once
+#include "job.hpp"
 #include "protos/render_server.pb.h"
+#include <memory>
 
 class RenderJobs {
 public:
-    void AddJob(std::string uuid, render_server::Status status);
+    void AddJob(std::string uuid, std::shared_ptr<Job> job);
     void UpdateJobStatus(std::string uuid, render_server::Status status);
     render_server::Status FetchJobStatus(std::string uuid);
+    void PrintJobs();
 
 private:
-    std::unordered_map<std::string, render_server::Status> jobMap;
+    std::unordered_map<std::string, std::shared_ptr<Job>> jobMap;
 };
