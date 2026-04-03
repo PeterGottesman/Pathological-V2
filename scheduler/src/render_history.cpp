c20f686b5ae2c2fa7e2691f46408567b455709c3
austinjohnson03 <amj102998@gmail.com>
2026-04-03 13:58:33 -0400

    finished render history implementation
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

---
+++ scheduler/src/render_history.cpp
@@ -0,0 +1,34 @@
+#include "render_history.hpp"
+
+RenderHistory::RenderHistory() = default;
+
+bool RenderHistory::addRender(std::shared_ptr<RenderRequest> req) {
+  if (!req)
+    return false;
+
+  this->history.emplace(req->getId(), req);
+  return true;
+}
+
+bool RenderHistory::deleteRender(const boost::uuids::uuid &id) {
+  return this->history.erase(id) > 0;
+}
+
+bool RenderHistory::updateStatus(const boost::uuids::uuid &id,
+                                 RenderStatus status) {
+  auto req = history.find(id);
+  if (req == history.end())
+    return false;
+
+  req->second->setStatus(status);
+  return true;
+}
+
+std::shared_ptr<RenderRequest>
+RenderHistory::getRenderRequest(const boost::uuids::uuid &id) const {
+  auto req = history.find(id);
+  if (req == history.end())
+    return nullptr;
+
+  return req->second;
+}
--- scheduler/src/render_history.hpp
+++ scheduler/src/render_history.hpp
@@ -1,24 +1,34 @@
 /*
  * Singleton class desgined to track all renders for status updates
- * Holds lightweight unordered map to track the status of all renders in the
+ * Holds unordered map to track the status of all renders in the
  * system. Could implement a datbase later on for more persistent data tracking.
  */
 #include "renderRequest.hpp"
 #include "renderStatus.hpp"
 #include <boost/uuid.hpp>
+#include <optional>
 #include <unordered_map>
 
 class RenderHistory {
 public:
-  RenderHistory();
-  ~RenderHistory();
+  static RenderHistory &getInstance() {
+    static RenderHistory instance;
+    return instance;
+  }
+
+  RenderHistory(const RenderHistory &) = delete;
+  RenderHistory &operator=(const RenderHistory &) = delete;
 
   bool addRender(std::shared_ptr<RenderRequest> req);
   bool deleteRender(const boost::uuids::uuid &id);
   bool updateStatus(const boost::uuids::uuid &id, RenderStatus status);
-  RenderStatus getStatus(const boost::uuids::uuid &id);
+  std::shared_ptr<RenderRequest>
+  getRenderRequest(const boost::uuids::uuid &id) const;
 
 private:
+  RenderHistory() = default;
+  ~RenderHistory() = default;
+
   std::unordered_map<boost::uuids::uuid, std::shared_ptr<RenderRequest>>
       history;
 };
