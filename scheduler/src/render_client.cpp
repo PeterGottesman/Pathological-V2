a935d843ef082b2b605e0c7b9bbb0fb0a5316bc8
austinjohnson03 <amj102998@gmail.com>
2026-04-03 14:02:15 -0400

    updated methods to match RenderRequest shared_ptr
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- scheduler/src/render_client.cpp
+++ scheduler/src/render_client.cpp
@@ -2,19 +2,19 @@
 
 // Assembles the client's payload using getter functions from renderRequest.cpp, sends it and presents the response back
 // from the server.
-std::string RenderWorkerClient::RenderJob(const RenderRequest& render) {
+std::string RenderWorkerClient::RenderJob(std::shared_ptr<RenderRequest> render) {
   // Data we are sending and getting back
   RenderJobRequest request;
   RenderJobResponse response;
   ClientContext context;
 
   // Adds data to request
-  request.set_scene_location(render.getSceneFileUrl());
-  request.set_output_name(render.getOutputFileName());
-  request.set_width(static_cast<uint32_t>(render.getWidth()));
-  request.set_height(static_cast<uint32_t>(render.getHeight()));
-  request.set_samples(static_cast<uint32_t>(render.getSamplesPerPixel()));
-  request.set_time(static_cast<float>(render.getAnimationRuntimeInFrames())); // ****I'm ASSUMING this is what is from the frontend
+  request.set_scene_location(render->getSceneFileUrl());
+  request.set_output_name(render->getOutputFileName());
+  request.set_width(static_cast<uint32_t>(render->getWidth()));
+  request.set_height(static_cast<uint32_t>(render->getHeight()));
+  request.set_samples(static_cast<uint32_t>(render->getSamplesPerPixel()));
+  request.set_time(static_cast<float>(render->getAnimationRuntimeInFrames())); // ****I'm ASSUMING this is what is from the frontend
 
   // The actual RPC.
   Status status = stub_->RenderJob(&context, request, &response);
