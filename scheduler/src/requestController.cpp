23693ea10636e82bb6792ad1c16712b1a2def013
austinjohnson03 <amj102998@gmail.com>
2026-04-07 13:02:46 -0400

    updated diff files from main branch
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- scheduler/src/requestController.cpp
+++ scheduler/src/requestController.cpp
@@ -5,6 +5,7 @@
 
 #include "renderRequest.hpp"
 #include "requestController.hpp"
+#include "scheduler.hpp"
 
 void RequestController::getStatus(
     const HttpRequestPtr &req,
@@ -65,7 +66,9 @@ void RequestController::createRenderRequest(
         .setSamplesPerPixel((*payload)["samples_per_pixel"].asInt())
         .setCreatedAtTimestamp(timestamp)
         .setSceneFileUrl((*payload)["scene_file_url"].asString())
-        .setOutputFileName((*payload)["output_file_name"].asString());
+        .setOutputFileName((*payload)["output_filename"].asString());
+
+    Scheduler::getInstance().addJob(render);
 
     auto resp = HttpResponse::newHttpJsonResponse(render.toJson());
     resp->setStatusCode(k201Created);
