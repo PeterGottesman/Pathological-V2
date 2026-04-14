1bc4bb743938d34bfc2921524078e96aa81c9f95
Hunter Ellenberger <helly10@outlook.com>
2026-04-14 00:16:46 -0400

    added CLI option to name worker

--- render_worker/src/main.cpp
+++ render_worker/src/main.cpp
@@ -46,15 +46,16 @@ void shutdownServer(){
 int main(int argc, char** argv) {
     CLI::App app{"Pathological - Vulkan Path Tracer"};
 
-    std::string worker_id = "test_client_1";
     std::string schedulerAddress;
     std::string renderServerAddress = "127.0.0.1";
     uint32_t renderServerPort = 50051;
+    std::string worker_id = "test_client_1";
     boost::uuids::random_generator random_gen;
 
     app.add_option("schedulerAddress", schedulerAddress, "Address and port to find scheduler at")->required();
     app.add_option("-a,--render-address", renderServerAddress, "Address of current machine running program")->default_val("127.0.0.1");
     app.add_option("-p,--port", renderServerPort, "Port to launch server")->default_val(50051);
+    app.add_option("-n, --name", worker_id, "Name that scheduler will refer to this render worker as");
     CLI11_PARSE(app, argc, argv);
 
     // Sends scheduler IP and port
